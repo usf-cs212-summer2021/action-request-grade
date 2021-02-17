4059c2b8bfcd779037c62adcab28f2941eb300ff
Sophie Engle <sjengle@cs.usfca.edu>
2021-02-16 18:27:38 -0800

    Adding constants

---
+++ constants.js
@@ -0,0 +1,19 @@
+exports.functionality = {
+  1: new Date(2021, 3, 2),
+  2: new Date(2021, 3, 30),
+  3: new Date(2021, 4, 20),
+  4: new Date(2021, 5, 11)
+};
+
+exports.design = {
+  1: new Date(2021, 3, 26),
+  2: new Date(2021, 4, 16),
+  3: new Date(2021, 5, 7)
+};
+
+exports.names = {
+  1: 'Inverted Index',
+  2: 'Partial Search',
+  3: 'Multithreading',
+  4: 'Search Engine'
+};
